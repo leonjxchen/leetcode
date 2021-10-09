50bdedbec9fbf8ec61079a7afef457e070d874f0
leonjxchen <1983919255@qq.com>
2021-10-09 00:26:04 -0400

    add 1653. Minimum Deletions to Make String Balanced

---
+++ c++/1653_Minimum_Deletions_to_Make_String_Balanced.cpp
@@ -0,0 +1,43 @@
+// You are given a string s consisting only of characters 'a' and 'b'​​​​.
+// 
+// You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+// 
+// Return the minimum number of deletions needed to make s balanced.
+// 
+//  
+// 
+// Example 1:
+// 
+// Input: s = "aababbab"
+// Output: 2
+// Explanation: You can either:
+// Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"), or
+// Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+// Example 2:
+// 
+// Input: s = "bbaaaaabb"
+// Output: 2
+// Explanation: The only solution is to delete the first two characters.
+//  
+// 
+// Constraints:
+// 
+// 1 <= s.length <= 105
+// s[i] is 'a' or 'b'​​.
+
+class Solution {
+public:
+    int minimumDeletions(std::string s) {
+        std::vector<int> dp(s.size() + 1);
+        int cnt = 0;
+        for (int i = 1; i <= s.size(); ++i) {
+            if (s[i - 1] == 'a') {
+                dp[i] = std::min(cnt, dp[i - 1] + 1);
+            } else {
+                dp[i] = dp[i - 1];
+                ++cnt;
+            }
+        }
+        return dp[s.size()];
+    }
+};
